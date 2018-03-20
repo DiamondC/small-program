6f71237adab95320f69b216f1f5583a4ffaf2a27
980760085@qq.com <980760085@qq.com>
2018-03-20 10:09:04 +0800

    first commit

---
+++ small program/ntripread.py
@@ -0,0 +1,31 @@
+import kivy
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
+class LoginScreen(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='latitude'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text='longitude'))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
+
+class MyApp(App):
+
+    def build(self):
+        return LoginScreen()
+
+if __name__ == '__main__':
+    MyApp().run()
+
+# class MyApp(App):
+#     def build(self):
+#         return Label(text='hello')
+# if __name__ == '__main__':
+#     MyApp().run()
